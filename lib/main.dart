eec2caaaeaa05e5cd43ebb8a050c356d92341cf0
samuelincoln1 <samuelincoln1@gmail.com>
2024-02-15 15:41:57 -0300

    att: firebase auth setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:teste/firebase_options.dart';
 import 'package:teste/views/login_page.dart';
 import 'package:teste/views/register_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MainApp());
 }
 
