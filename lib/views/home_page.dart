d08d424dbf08fbd631ceb84d55ccfb42807ee65f
samuel <samuelincoln1@gmail.com>
2024-02-17 19:40:59 -0300

    att: firestore crud notes

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:teste/services/firestore.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,8 +12,39 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final firestoreService = FirestoreService();
+  final textController = TextEditingController();
+
+
   void signOut() async {
     await FirebaseAuth.instance.signOut();
+  }
+
+  void openNoteBox(String? docID) {
+    showDialog(
+      context: context, 
+      builder: (context) =>  AlertDialog(
+        content: TextField(
+          controller: textController,
+        ),
+        actions: [
+          ElevatedButton(
+            onPressed: () async {
+              if (docID == null) {
+                 await firestoreService.addNote(textController.text);
+              } else {
+                await firestoreService.updateNote(docID, textController.text);
+              }
+             
+              textController.clear();
+              if (!mounted) return;
+              Navigator.pop(context);
+            }, 
+            child: const Text('adicionar'),
+          ),
+        ],
+      ),
+    );
   }
 
   final user = FirebaseAuth.instance.currentUser!;
@@ -19,6 +52,12 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => openNoteBox(null),
+        backgroundColor: const Color.fromARGB(255, 255, 93, 81),
+        child: const Icon(Icons.add),
+       
+      ),
       body: Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
@@ -44,19 +83,58 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ],
                 ),
-                Container(
-                  width: 300, 
-                  height: 50, 
-                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(8),  color: Colors.red.shade300, ),
-                  child: const Center(child: Text('oi', style:TextStyle(color:Colors.white, fontSize: 23,),),),
+                StreamBuilder(
+                stream: firestoreService.getNotesStream(), 
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    List notesList = snapshot.data!.docs;
+                    return Expanded(
+                      child: ListView.builder(
+                        itemCount: notesList.length,
+                        itemBuilder: ((context, index) { 
+                          DocumentSnapshot document = notesList[index];
+                          String docID = document.id;
+                      
+                          Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+                          String noteText = data['note'];
+                          
+                          return ListTile(
+                            title: Text(noteText),
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                IconButton(
+                                  onPressed: () => openNoteBox(docID),
+                                  icon: const Icon(Icons.edit),
+                                ),
+                                IconButton(
+                                  onPressed: () => firestoreService.deleteNote(docID),
+                                  icon: const Icon(Icons.delete),
+                                ),
+                              ],
+                            ),
+                          );
+                      
+                        }
+                        ),
+                      ),
+                    );
+                  } else {
+                    return const Text('Você ainda não possui nenhuma anotação');
+                  }
+                }
                 ),
-                Text('Logado como ${user.email}'),
-                Text('Username: ${user.displayName}'),
+
               ],
             ),
           ),
         ),
+        
       ),
     );
   }
 }
+
+
+
+        
